703d23eacbde0285da69be5c42983730af75d6e2
davido12 <davido_12@hotmail.com>
2019-02-06 19:02:57 +0000

    fix an error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int main (){
   char a [50];
-  strcpy(a, "Programming is like building a multilingual puzzle");
+  strcpy(a, "\"Programming is like building a multilingual puzzle");
   puts(a);
   return 0;
 }
