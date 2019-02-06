754611266d8c61e448391e05f8ff9c871764054e
davido12 <davido_12@hotmail.com>
2019-02-06 21:45:49 +0000

    hot fix break line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  *main - prints string
- *Return: (0) (Done)
+ *Return: Always (0) (Done)
  **/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
