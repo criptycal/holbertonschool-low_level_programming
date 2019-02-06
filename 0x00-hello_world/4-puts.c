b3157d9463e58bcffd110961ba26fa7a5cb786bd
davido12 <davido_12@hotmail.com>
2019-02-06 19:21:37 +0000

    4 script fix some errors in the syntaxis

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-int main (){
-  char a [50];
-  strcpy(a, "\"Programming is like building a multilingual puzzle");
-  puts(a);
-  return 0;
+int main(void)/*main function*/
+{
+char a[50];
+strcpy(a, "\"Programming is like building a multilingual puzzle");
+puts(a);
+return (0);
 }
