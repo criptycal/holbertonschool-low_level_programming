5ae83bbddc5895730c748ab18b1a4a5a8a634e69
davido12 <davido_12@hotmail.com>
2019-02-12 19:32:46 +0000

    hot fix, betty recomendations

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,17 @@
 #include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  char holbertonSchool[]="Holberton\n";
-  int i=0;
-  while(i<12)
-    {
-      _putchar(holbertonSchool[i]);
-      i++;
-    }
-  return(0);
+char holbertonSchool[] = "Holberton\n";
+int i = 0;
+while (i < 12)
+{
+_putchar (holbertonSchool[i]);
+i++;
 }
-
+return (0);
+}
