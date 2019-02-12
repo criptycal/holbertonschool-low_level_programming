e0c44974f3b86ce7508216ffeebc1705030fe8a8
davido12 <davido_12@hotmail.com>
2019-02-12 20:05:44 +0000

    hot fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,15 +7,11 @@
 
 int main(void)
 {
-_putchar ('H');
-_putchar ('o');
-_putchar ('l');
-_putchar ('b');
-_putchar ('e');
-_putchar ('r');
-_putchar ('t');
-_putchar ('o');
-_putchar ('n');
-_putchar ('\n');
+char holberton[13] = "Holberton\n";
+int i = 0;
+while (i < 13)
+{
+_putchar (holberton[i]);
+}
 return (0);
 }
